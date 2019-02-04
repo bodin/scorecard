52cadbe410fd2bf117224dcee171acb76810a03f
Derek Bodin <derek.bodin@gmail.com>
2019-02-03 21:49:40 -0600

    starting first component

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,18 @@
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <FiestyDice/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import FiestyDice from './components/FiestyDice'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FiestyDice
   },
   data () {
     return {

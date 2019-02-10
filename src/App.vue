30682a8836373a66cd4963df16927c25fb8df6ee
Derek Bodin <derek.bodin@gmail.com>
2019-02-10 09:14:56 -0600

    public facing

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  <b-container>
-      <b-row>
-          <b-col></b-col>
-          <b-col>
-            <FiestyDice/>
-          </b-col>
-          <b-col></b-col>
-      </b-row>
+<div>
+  <b-navbar toggleable="md" type="dark" variant="info">
+
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-navbar-brand href="#">{{game}}</b-navbar-brand>
+
+    <b-collapse is-nav id="nav_collapse">
+
+      <b-navbar-nav>
+        <b-nav-item href="#">New Game</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="Select Game" right>
+          <b-dropdown-item href="#">Fiesty Dive</b-dropdown-item>
+          <b-dropdown-item href="#">Yotze</b-dropdown-item>          
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
+  <b-container class="mt-5">  
+    <b-row>
+        <b-col lg="3" sm="0"></b-col>
+        <b-col lg="6" sm="12">
+          <FiestyDice/>
+        </b-col>
+        <b-col lg="3" sm="0"></b-col>
+    </b-row>
   </b-container>
+</div>
 </template>
 
 <script>
@@ -20,7 +44,7 @@ export default {
   },
   data () {
     return {
-      //
+      game: "Fiesty Dice"
     }
   }
 }
--- src/components/FiestyDice.vue
+++ src/components/FiestyDice.vue
@@ -1,19 +1,28 @@
 <template>
   <b-container>
-        <b-row :key="index" v-for="(item, index) in this.rows"> 
-          <b-col>       
-            <Row 
-              :display="item.display"
-              :initialScore="item.score"
-              @score="score(index, $event)"                
-            ></Row>
-          </b-col>
-        </b-row>      
-        <b-row>
-          <b-col>       
-            {{total()}}
-          </b-col>            
-        </b-row>
+    <b-row>
+      <b-col>
+        <b-card>
+          <h6 slot="header">Rolls Taken {{rolls}}</h6>
+          
+          <Row :key="index" v-for="(item, index) in this.rows"
+            :display="item.display"
+            :initialScore="item.score"
+            @score="score(index, $event)"                
+          ></Row>                       
+          
+          <div slot="footer">
+            <h6>
+              Bonus {{bonus()}}
+            </h6>
+            <h4>
+              Total {{total()}}
+            </h4>
+          </div>          
+        </b-card>
+      </b-col>
+    </b-row>
+      
   </b-container>
 </template>
 
@@ -30,22 +39,24 @@
       }
     },
     data: () => ({
+      rolls:5,
       rows: [
         {key:0, display: 'Dice 6', score:null},
         {key:1, display: 'Dice 5', score:null},
         {key:2, display: 'Dice 4', score:null},
-        {key:1, display: 'Dice 3', score:null},
-        {key:1, display: 'Dice 2', score:null},
-        {key:1, display: 'Dice 1', score:null},
-        {key:1, display: 'Run', score:null},
-        {key:1, display: 'Set', score:null},
-        {key:1, display: 'Sum', score:null},
-        {key:1, display: 'Red', score:null}
+        {key:3, display: 'Dice 3', score:null},
+        {key:4, display: 'Dice 2', score:null},
+        {key:5, display: 'Dice 1', score:null},
+        {key:6, display: 'Run', score:null},
+        {key:7, display: 'Set', score:null},
+        {key:8, display: 'Sum', score:null},
+        {key:9, display: 'Red', score:null}
       ],
-
-      rolls: 0,
+      bonus: function(){
+        return 5 * (30-this.rolls);
+      },
       total: function(){
-        return 5 * (30-this.rolls) + Object.values(this.rows).reduce((t, n) => t + n.score , 0);
+        return this.bonus() + Object.values(this.rows).reduce((t, n) => t + n.score , 0);
       }
     })
   }
